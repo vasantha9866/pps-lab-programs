6d28f70853c224cd4881865236530972b390c945
vasantha9866 <85933121+vasantha9866@users.noreply.github.com>
2021-07-20 10:45:53 +0530

    Add files via upload

---
+++ alphabet-or-not using-co.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{ 
+      char ch;
+      printf("enter value");
+      scanf("\n%c",&ch);
+      ((ch>='a'&&ch<='z')\\(ch>='A'&&ch,='Z'))?printf("its is alphabet"):("not an alphabet");
+}
