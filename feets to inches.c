56edfa0a0d6ab1662e0ae0f140cb092d38c2b8b0
vasantha9866 <85933121+vasantha9866@users.noreply.github.com>
2021-07-20 10:48:17 +0530

    Add files via upload

---
+++ feets to inches.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+           int ft,in,no_of_inches;
+	      printf("\n enter value");
+	      scanf("\n%d"&ft);
+	      no_of_inches=(ft*12);
+	      printf("no_of_inches=%d",no_of_inches);
+}
+
