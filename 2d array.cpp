60bd6971b29c224167d0f3fd6cb6e631fb8984c0
vasantha9866 <85933121+vasantha9866@users.noreply.github.com>
2021-07-20 10:45:07 +0530

    Add files via upload

---
+++ 2d array.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	     int a[10][10],i,j,r,c;
+	     printf("eter the number the number of rows and colums:");
+	     scanf("%d%d",&r,&c);
+	     for(i=0;i<r;i++){
+	             for(j=0;j<c;j++)
+	             { 
+	                   scanf("%d",&a[i][j]);
+	            
+			   }
+			}   
+            for(i=0;i<r;i++)
+            {
+             	     for(j=0;j<c;j++)
+             	     {
+             	     	      printf("%4d",a[1][j]);
+             	     	      
+					 }
+					  printf("\n"0;
+					  
+	   }
+				 return 0;
+}
