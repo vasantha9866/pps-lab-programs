5ba24c79ae6c7fc9499703cee1856b05e9b724da
vasantha9866 <85933121+vasantha9866@users.noreply.github.com>
2021-07-20 10:44:45 +0530

    Add files via upload

---
+++ 0ne's digit of an integer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+           int a;
+           printf("\n enter the value of a");
+           scanf("\n%d",&a);
+           printf("\na=%d",a%10);
+           
+}
+
